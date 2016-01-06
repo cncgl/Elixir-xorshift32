db3492750ea596deed449ba914458b9cdb1d1346
cncgl <concigel@gmail.com>
2016-01-07 00:17:09 +0900

    use pipeline

--- lib/grandom.ex
+++ lib/grandom.ex
@@ -1,9 +1,17 @@
 defmodule Grandom do
+  use Bitwise
   def rand(seed) do
-    use Bitwise
-    y = seed
-    y = y ^^^ (y <<< 13)
-    y = y ^^^ (y >>> 17)
-    y = y ^^^ (y <<< 15)
+    seed
+    |> xor_right_shift(13)
+    |> xor_left_shift(17)
+    |> xor_right_shift(15)
+  end
+
+  defp xor_right_shift(x, num) do
+    x ^^^ (x <<< num)
+  end
+
+  defp xor_left_shift(x, num) do
+    x ^^^ (x >>> num)
   end
 end
